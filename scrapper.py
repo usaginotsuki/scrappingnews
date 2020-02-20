9db74aebd84a8f65f56e7b029e0ff165d0fa0866
Jean <jcchachalo@gmail.com>
2020-02-19 23:24:07 -0500

    El Merci
    Crawling basico de paginas de https://elmercio.com/

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,48 @@
-import urllib.request
-datos = urllib.request.urlopen('https://www.openwebinars.net').read().decode()
-import sys
-!{sys.executable} -m pip install beautifulsoup4
+import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+URL = 'https://elmercio.com/politica/'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+project_href = [i['href'] for i in soup.find_all('a', href=True)]
+print(project_href)
+
+for i in range(2,4):
+    URL = 'https://elmercio.com/politica/page'+str(i)
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    project_href = [i['href'] for i in soup.find_all('a', href=True)]
+    print(project_href)
+
+URL = 'https://elmercio.com/sociedad/'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+project_href = [i['href'] for i in soup.find_all('a', href=True)]
+print(project_href)
+
+for i in range(2,3):
+    URL = 'https://elmercio.com/sociedad/'+str(i)
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    project_href = [i['href'] for i in soup.find_all('a', href=True)]
+    print(project_href)
+
+URL = 'https://elmercio.com/internacional/'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+project_href = [i['href'] for i in soup.find_all('a', href=True)]
+print(project_href)
+
+for i in range(2,3):
+    URL = 'https://elmercio.com/internacional/'+str(i)
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+    project_href = [i['href'] for i in soup.find_all('a', href=True)]
+    print(project_href)
